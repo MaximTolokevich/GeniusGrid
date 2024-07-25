a34a6efa433ebe3b7e602d4b3cfb5ec3f25bf6d2
XhF1lter <eugene.kovalchuk043@gmail.com>
2024-07-26 01:24:02 +0300

    Refactor user service

--- Front/src/app/user/user.interceptor.ts
+++ Front/src/app/user/user.interceptor.ts
@@ -1,20 +1,58 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest} from "@angular/common/http";
 import { inject } from "@angular/core";
 import { UserService } from "./user.service";
+import {catchError, switchMap, throwError} from "rxjs";
+
+let isRefreshing = false
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = inject(UserService).token;
+  const userService = inject(UserService)
+  const token = userService.token
 
-  console.log(token)
-  if (!token) return next(req);
+  if (!token) return next(req)
 
-  req = req.clone({
+  if (isRefreshing) {
+    return refreshAdnProceed(userService, req, next)
+  }
+
+  return next(addToken(req, token))
+    .pipe(
+      catchError(error => {
+        if(error.status === 403) {
+          return refreshAdnProceed(userService, req, next)
+        }
+
+        return throwError(error)
+      })
+    )
+}
+
+const refreshAdnProceed = (
+  userService: UserService,
+  req: HttpRequest<any>,
+  next: HttpHandlerFn
+) => {
+  if (!isRefreshing) {
+    isRefreshing = true
+    return userService.refreshAuthToken()
+      .pipe(
+        //TODO access token
+        switchMap((res) => {
+          isRefreshing = false
+          return next(addToken(req, res.token))
+        })
+      )
+  }
+
+  return next(addToken(req, userService.token!))
+}
+
+const addToken = (req: HttpRequest<any>, token: string) => {
+  return req.clone({
     setHeaders: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': 'http://localhost:4200',
       Authorization: `Bearer ${token}`
     }
   });
-
-  return next(req);
-};
+}
--- Front/src/app/user/user.service.ts
+++ Front/src/app/user/user.service.ts
@@ -1,23 +1,26 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import {tap} from "rxjs";
+import {catchError, tap, throwError} from "rxjs";
 import {RegistrationResponce} from "./auth.interface";
 import {CookieService} from "ngx-cookie-service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   http: HttpClient = inject(HttpClient)
+  router = inject(Router)
   cookieServise = inject(CookieService)
   baseApiUrl = 'https://localhost:7000/User/'
 
-  token: string | null = '';
-  // refreshToken: string | null = null
+  token: string | null = ""
+  refreshToken: string | null = ""
 
   get isAuth() {
     if (!this.token) {
       this.token = this.cookieServise.get('token')
+      this.refreshToken = this.cookieServise.get('refreshToken')
     }
     return !!this.token
   }
@@ -28,14 +31,45 @@ export class UserService {
       payload
     ).pipe(
       tap(val => {
-        this.token = val.token
-
-        this.cookieServise.set('token', this.token)
+        this.saveTokens(val)
       })
     )
   }
 
   getAll() {
     return this.http.post(`${this.baseApiUrl}GetAll`, "")
+  }
+
+  refreshAuthToken() {
+    return this.http.post<RegistrationResponce>(
+      `${this.baseApiUrl}RefreshToken`,
+      {
+        refreshToken: this.refreshToken
+      }
+      ).pipe(
+        tap(val => {
+          this.saveTokens(val)
+        }),
+        catchError(err => {
+          this.logout()
+          return throwError(err)
+        })
+    )
+  }
+
+  logout() {
+    this.cookieServise.deleteAll()
+    this.token = ""
+    this.refreshToken = ""
+    this.router.navigate(['/login'])
+  }
+
+// TODO new interface for tokens and responce from AUTHMethods
+  saveTokens(res: RegistrationResponce) {
+    this.token = res.token
+    // this.refreshToken = val.refreshToken
+
+    this.cookieServise.set('token', this.token)
+    // this.cookieServise.set('refreshToken', this.refreshToken)
   }
 }
